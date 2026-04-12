6d9911f1ff268642326f4cbaced4c09f001b3a5f
Vikram Thakur <vikramkk380@gmail.com>
2026-04-12 21:00:53 +0530

    connect backend

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
     baseURL: "http://localhost:5000/api"
 });
 
-// attach token automatically
+
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
     if (token) {
