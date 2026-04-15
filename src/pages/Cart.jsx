310a7c4cad8e9bfb533f0d7f4dc99350105dd3ec
Vikram Thakur <vikramkk380@gmail.com>
2026-04-16 01:38:33 +0530

    update quantity

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,52 +8,67 @@ export default function Cart() {
   const [err, setErr] = useState(null);
 
   useEffect(() => {
-    const getCart = async () => {
-      try {
-        setLoading(true);
-
-        const res = await API.get("/cart");
-        console.log(res.data);
-        const items = res.data.cart.items || [];
-
-        if (items.length === 0) {
-          setErr("Your cart is empty. Continue shopping 🛒");
-          return;
-        }
-
-        setCart(items);
-
-        const totalPrice = items.reduce(
-          (acc, item) => acc + item.product.price * item.quantity,
-          0,
-        );
-
-        setTotal(totalPrice);
-      } catch (error) {
-        setErr(error.response?.data?.message || "Error fetching cart");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     getCart();
   }, []);
+  const getCart = async () => {
+    try {
+      setLoading(true);
+
+      const res = await API.get("/cart");
+      console.log(res.data);
+      const items = res.data.cart.items || [];
+
+      if (items.length === 0) {
+        setErr("Your cart is empty. Continue shopping 🛒");
+        return;
+      }
+
+      setCart(items);
+
+      const totalPrice = items.reduce(
+        (acc, item) => acc + item.product.price * item.quantity,
+        0,
+      );
+
+      setTotal(totalPrice);
+    } catch (error) {
+      setErr(error.response?.data?.message || "Error fetching cart");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const HandleRemove = async (id) => {
     console.log(id);
     try {
-      await API.delete("/cart/remove", { data: { productId: id } });
+      const res = await API.delete("/cart/remove", { data: { productId: id } });
       const updatedCart = cart.filter((item) => item.product._id !== id);
       setCart(updatedCart);
 
       if (updatedCart.length === 0) setErr("Your cart is empty. 🛒");
       console.log(res.data);
     } catch (error) {
-      return setErr(
+      setErr(
         error.response?.data?.message ||
           `error while removing the item from the cart`,
       );
     }
+  };
+
+  const handleQuantity = async (id, opp, currQty) => {
+    let qty = currQty;
+    if (opp === "add") {
+      qty = qty + 1;
+    } else if (opp === "sub") {
+      qty = qty - 1;
+    }
+    console.log(qty);
+    const res = await API.patch("/cart/update", {
+      productId: id,
+      quantity: qty,
+    });
+    getCart();
+    // setCart(res.data.cart);
   };
 
   if (loading) return <h1>Loading...</h1>;
@@ -67,7 +82,21 @@ export default function Cart() {
           <h1>{item.product.name}</h1>
           <p>{item.product.description}</p>
           <h2>₹{item.product.price}</h2>
+          <button
+            onClick={() => {
+              handleQuantity(item.product._id, "sub", item.quantity);
+            }}
+          >
+            -
+          </button>
           <p>Qty: {item.quantity}</p>
+          <button
+            onClick={() => {
+              handleQuantity(item.product._id, "add", item.quantity);
+            }}
+          >
+            +
+          </button>
           <button
             onClick={() => {
               HandleRemove(item.product._id);
